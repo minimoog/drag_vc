23fa3558c8d91f18dd25dc243050dec60fb9ffbd
Toni Jovanoski <minimoog77@gmail.com>
2019-10-08 20:30:20 +0200

    Initial Commit

---
+++ drag_vc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  drag_vc
+//
+//  Created by Antonie Jovanoski on 10/8/19.
+//  Copyright © 2019 Antonie Jovanoski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
