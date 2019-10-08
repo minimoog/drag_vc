2572f4dfd7f350c836ee8b4b3400ca6748ed040a
Toni Jovanoski <minimoog77@gmail.com>
2019-10-09 00:22:43 +0200

    Add draggable view with constraints

--- drag_vc/ViewController.swift
+++ drag_vc/ViewController.swift
@@ -10,11 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var trailingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var topConstraint: NSLayoutConstraint!
+    @IBOutlet weak var heightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var widthConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func panenka(_ panRecognizer: UIPanGestureRecognizer) {
+        guard let view = panRecognizer.view else { return }
+        
+        let translation = panRecognizer.translation(in: view)
+    
+        trailingConstraint.constant -= translation.x
+        
+        if trailingConstraint.constant < 20 {
+            trailingConstraint.constant = 20
+        }
+        
+        topConstraint.constant += translation.y
+        
+        if topConstraint.constant < 20 {
+            topConstraint.constant = 20
+        }
+        
+        panRecognizer.setTranslation(.zero, in: view)
+    }
+    
+    @IBAction func pinch(_ pinchGestureRecognizer: UIPinchGestureRecognizer) {
+        guard let view = pinchGestureRecognizer.view else { return }
+        
+        let scale = pinchGestureRecognizer.scale
+        
+        heightConstraint.constant *= scale
+        
+        if heightConstraint.constant < 100 { heightConstraint.constant = 100 }
+        if heightConstraint.constant > 512 { heightConstraint.constant = 512 }
+        
+        widthConstraint.constant *= scale
+        
+        if widthConstraint.constant < 100 { widthConstraint.constant = 100 }
+        if widthConstraint.constant > 512 { widthConstraint.constant = 512 }
+        
+        pinchGestureRecognizer.scale = 1.0
+    }
+    
+    
+    @IBAction func tachi(_ sender: UIButton) {
+        
+    }
 }
 
